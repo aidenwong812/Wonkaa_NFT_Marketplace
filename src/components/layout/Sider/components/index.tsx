bca5542541a1329ac84525d78a4bfea638d052c3
ekans111 <kobayasitorao111@gmail.com>
2024-05-28 16:51:55 +0900

    update navbar

--- src/components/layout/Sider/components/index.tsx
+++ src/components/layout/Sider/components/index.tsx
@@ -23,57 +23,15 @@ const _renderLinkItem = (props: any) => {
         onClick={() => handleNavigate(props._url)}
         className={`relative flex items-center py-3 focus:bg-[#50FFFF0D] ${
           props.siderWidth < 1600 ? "justify-center" : "px-5 w-full"
-        } text-white hover:border-[#F7F9FC] hover:bg-[#50FFFF0D] cursor-pointer text-sm rounded-[10px] font-ttfirs ${
+        } hover:text-[#53FAFB] hover:border-[#F7F9FC] hover:bg-[#50FFFF0D] cursor-pointer text-sm rounded-[10px] font-ttfirs ${
           props._url === "/home" && props._url === props.pathname
-            ? "bg-[#50FFFF0D] border-none"
+            ? "bg-[#50FFFF0D] text-[#53FAFB] border-none px-5"
             : props._url !== "/home" && props.pathname.includes(props._url)
-            ? "bg-[#50FFFF0D] border-none"
-            : ""
+            ? "bg-[#50FFFF0D] text-[#53FAFB] border-none px-5"
+            : "text-white px-5"
         }`}
       >
-        {props._url === "/home" && props._url === props.pathname ? (
-          <div
-            className={`${props.siderWidth > 1600 ? `mr-[15px]` : ""} w-[20px]`}
-          >
-            <Image
-              src={props._clickedIcon}
-              height={0}
-              width={0}
-              alt=""
-              className="w-[20px] h-auto"
-            />
-          </div>
-        ) : props._url !== "/home" && props.pathname.includes(props._url) ? (
-          <div
-            className={`${props.siderWidth > 1600 ? `mr-[15px]` : ""} w-[20px]`}
-          >
-            <Image
-              src={props._clickedIcon}
-              width={0}
-              height={0}
-              alt=""
-              className="w-[20px] h-auto"
-              priority={true}
-            />
-          </div>
-        ) : (
-          <div
-            className={`${props.siderWidth > 1600 ? `mr-[15px]` : ""} w-[18px]`}
-          >
-            <Image
-              src={props._icon}
-              height={0}
-              width={0}
-              alt=""
-              className="w-[18px] h-auto"
-            />
-          </div>
-        )}
-        {props.siderWidth > 1600 ? (
-          <>
-            {props._name}
-          </>
-        ) : null}
+        <div>{props._name}</div>
       </li>
     </div>
     // </div>
