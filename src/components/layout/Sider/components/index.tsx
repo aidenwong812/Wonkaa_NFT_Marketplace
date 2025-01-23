5acc52194da0b3ef2ac63cebe4178da0dc983cbf
aidenwong812 <wongaiden812@gmail.com>
2025-01-23 00:24:42 -0500

    fix title and text color

--- src/components/layout/Sider/components/index.tsx
+++ src/components/layout/Sider/components/index.tsx
@@ -12,13 +12,13 @@ const _renderLinkItem = (props: any) => {
   return (
     <li
       onClick={() => handleNavigate(props._url)}
-      className={`relative flex items-center py-3 focus:bg-gradient-to-r focus:from-red-500 focus:via-green-500 focus:to-yellow-500 justify-center text-white
-         hover:text-[#53FAFB] hover:bg-gradient-to-r hover:from-red-500 hover:via-green-500 hover:to-yellow-500 cursor-pointer text-sm rounded-[10px] font-ttfirs ${
+      className={`relative flex items-center py-3 focus:bg-gradient-to-r focus:from-red-500 focus:via-green-500 focus:to-yellow-500 justify-center text-black
+         hover:text-[#53FAFB] hover:bg-gradient-to-r hover:from-red-500 hover:via-green-500 hover:to-yellow-500 cursor-pointer text-sm font-semibold rounded-[10px] font-ttfirs ${
            props._url === "/home" && props._url === props.pathname
              ? "bg-gradient-to-r from-purple-500 via-pink-500 to-green-500 border-none px-5"
              : props._url !== "/home" && props.pathname.includes(props._url)
              ? "bg-gradient-to-r from-purple-500 via-pink-500 to-green-500 border-none px-5"
-             : "text-white px-5"
+             : "text-black px-5"
          }`}
     >
       <div>{props._name}</div>
--- src/components/layout/Sider/sider/index.tsx
+++ src/components/layout/Sider/sider/index.tsx
@@ -11,9 +11,9 @@ const SiderList = (props: any) => {
   const { walletID } = useWallet();
 
   return (
-    <ul className="flex text-[#D4D4D4] text-md mt-[10px] overflow-auto mx-auto gap-2 my-0">
+    <ul className="flex text-black text-md mt-[10px] overflow-auto mx-auto gap-2 my-0">
       <ListComponent
-        _name="Golden Ticket in place of Marketplace"
+        _name="Wonkaa Crypto Bar"
         _icon="/icon/home_white.svg"
         _clickedIcon="/icon/home_blue.svg"
         _url="/golden-ticket"
